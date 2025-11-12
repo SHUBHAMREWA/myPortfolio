1fc035cfd2d747cd76d9ef9e375c071416912c18
shubham <shubhamrewamp17@gmail.com>
2025-11-13 00:37:39 +0530

    date added

--- src/Componets/Education/Education.jsx
+++ src/Componets/Education/Education.jsx
@@ -70,7 +70,7 @@ return (
                     </h4>
                   </div>
                   {/* Date at the bottom */}
-                  <p className="text-sm text-gray-600 dark:text-gray-500 mt-2">{education.date}</p>
+                  {/* <p className="text-sm text-gray-600 dark:text-gray-500 mt-2">{education.date}</p> */}
                 </div>
               </div>
 
