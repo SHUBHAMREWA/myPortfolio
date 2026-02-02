83d684508b4ad08d8f320ca4c6660bd9ad8a97d7
shubham kushwaha <shubhamrewamp17@gmail.com>
2026-02-02 16:32:54 +0530

    [chore] added loading lazy in img

--- src/Componets/Work/Work.jsx
+++ src/Componets/Work/Work.jsx
@@ -52,7 +52,7 @@ const Work = () => {
                                     hover:shadow-primary-green hover:translate-y-2  transition-transform duration-300"
                             >
                                 <div className="p-4">
-                                    <img src={el.image} alt={el.alt || el.title} title={el.title} className="w-full h-40 object-cover rounded-xl" />
+                                    <img src={el.image} alt={el.alt || el.title} title={el.title} loading="lazy" className="w-full h-40 object-cover rounded-xl" />
                                 </div>
                              
                              <div className="p-1 text-center">
@@ -101,7 +101,7 @@ const Work = () => {
 
                 <div className="flex flex-col">
                     <div className="w-100 flex justify-center bg-card-light dark:bg-gray-900 px-4">
-                          <img  height={120}
+                          <img  height={120} loading="lazy"
                           className=" w-[80%] sm:w-[90%]  object-contain rounded-xl shadow-2xl "
                           src={selectedProject.image} alt={selectedProject.alt || selectedProject.title} title={selectedProject.title} />
                     </div>
